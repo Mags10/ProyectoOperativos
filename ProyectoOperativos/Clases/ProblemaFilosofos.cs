5446ff531c3b66d6d118255463ed1aefd283626d
MIGUEL ALEJANDRO GUTIERREZ SILVA <A330153@alumnos.uaslp.mx>
2024-05-22 14:52:42 -0600

    Class re-organitation
    1. Removal of `ClassDiagram1.cd`, `Filosofo.cs`, `Palillo.cs`, `Program.cs`, `VistaFilosofos.Designer.cs`, `VistaFilosofos.cs`, and `VistaFilosofos.resx` files.
    2. Update of `ProyectoOperativos.csproj` file to reflect new directory structure and addition of new files.
    3. Addition of new classes and methods in `Filosofo.cs`, `Palillo.cs`, `Program.cs`, and `VistaFilosofos.cs` files.
    4. Modification of `VistaFilosofos.cs` file to handle button click events and create a new instance of `ProblemaFilosofos`.
    5. Addition of new namespace `ProyectoOperativos`, new partial class `VistaFilosofos`, and several new methods and UI components.
    6. Addition of XML schema for a .resx file.

---
+++ ProyectoOperativos/Clases/ProblemaFilosofos.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoOperativos
+{
+    public class ProblemaFilosofos
+    {
+        private VistaFilosofos vista;
+        private List<Palillo> palillos = null;
+        private List<Filosofo> filosofos = null;
+        private bool initialized = false;
+
+        private int minSleepFilosofo = 2000;
+        private int maxSleepFilosofo = 5000;
+        private int minSleepPalillo = 1000;
+        private int maxSleepPalillo = 2000;
+
+        public ProblemaFilosofos(VistaFilosofos vista)
+        {
+            this.vista = vista;
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            if (initialized) return;
+            initialized = true;
+            palillos = new List<Palillo>();
+            filosofos = new List<Filosofo>();
+            for (int i = 0; i < 5; i++)
+            {
+                palillos.Add(new Palillo(i, vista));
+                filosofos.Add(new Filosofo(palillos, i, vista));
+                palillos[i].minSleep = minSleepPalillo;
+                palillos[i].maxSleep = maxSleepPalillo;
+                filosofos[i].minSleep = minSleepFilosofo;
+                filosofos[i].maxSleep = maxSleepFilosofo;
+            }
+        }
+
+        public void SetSleepFilosofo(int min, int max)
+        {
+            minSleepFilosofo = min;
+            maxSleepFilosofo = max;
+            foreach (Filosofo f in filosofos)
+            {
+                f.minSleep = minSleepFilosofo;
+                f.maxSleep = maxSleepFilosofo;
+            }
+        }
+
+        public void SetSleepPalillo(int min, int max)
+        {
+            minSleepPalillo = min;
+            maxSleepPalillo = max;
+            foreach (Palillo p in palillos)
+            {
+                p.minSleep = minSleepPalillo;
+                p.maxSleep = maxSleepPalillo;
+            }
+        }
+
+        public void Start()
+        {
+            if (!initialized) Initialize();
+            foreach (Filosofo f in filosofos) f.Start();
+        }
+
+        public void Suspend()
+        {
+            foreach (Filosofo f in filosofos) f.Suspend();
+        }
+
+        public void Resume()
+        {
+            foreach (Filosofo f in filosofos) f.Resume();
+        }
+
+        public void Abort()
+        {
+            foreach (Filosofo f in filosofos) f.Abort();
+            palillos = null;
+            filosofos = null;
+            initialized = false;
+        }
+    }
+}
---
+++ ProyectoOperativos/Clases/RandomValues.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProyectoOperativos
+{
+    public static class RandomValues
+    {
+        private static Random r = new Random();
+        public static int RandomInt(int min, int max)
+        {
+            return r.Next(min, max);
+        }
+        public static void RandomSleep(int min, int max)
+        {
+            Thread.Sleep(RandomInt(min, max));
+        }
+    }
+}
